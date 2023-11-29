3d59beb218c025ce7034938b6b1151ce9125bdd8
Syeda Umme Ayman <aymanshoity@gmail.com>
2023-11-29 10:42:04 +0600

    coupon sectionshowing is done

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000
 
 app.use(
   cors({
-    origin: ['http://localhost:5173','http://localhost:5174', 'https://enmmedia.web.app'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://enmmedia.web.app'],
     credentials: true,
   }),
 )
@@ -36,13 +36,13 @@ const verifyToken = (req, res, next) => {
   })
 }
 
-const verifyAdmin=async(req,res,next)=>{
-  const email=req.decoded.email;
-  const query= {email:email}
-  const accHolder=await accountHolderCollection.findOne(query)
+const verifyAdmin = async (req, res, next) => {
+  const email = req.decoded.email;
+  const query = { email: email }
+  const accHolder = await accountHolderCollection.findOne(query)
 
-  const isAdmin=accHolder?.role==="admin"
-  if(!isAdmin){
+  const isAdmin = accHolder?.role === "admin"
+  if (!isAdmin) {
     return res.status(403).send({ message: 'Forbidden Access' })
   }
   next()
@@ -76,6 +76,7 @@ const apartmentCollection = client.db("Apex-Kare-Estate").collection("apartments
 const accountHolderCollection = client.db("Apex-Kare-Estate").collection("accountHolders");
 const agreementRequestsCollection = client.db("Apex-Kare-Estate").collection("agreementRequests");
 const announcementsCollection = client.db("Apex-Kare-Estate").collection("announcements");
+const couponsCollection = client.db("Apex-Kare-Estate").collection("coupons");
 
 
 
@@ -111,36 +112,54 @@ app.post('/accountHolders', async (req, res) => {
   const result = await accountHolderCollection.insertOne(accountHolder)
   res.send(result)
 })
-app.get('/accountHolders',verifyToken,verifyAdmin, async (req, res) => {
+app.get('/accountHolders', verifyToken, verifyAdmin, async (req, res) => {
   const cursor = await accountHolderCollection.find().toArray()
   res.send(cursor)
 })
 
-app.post('/agreementRequests', verifyToken,async (req, res) => {
+app.post('/agreementRequests', verifyToken, async (req, res) => {
   const agreementRequest = req.body;
   const result = await agreementRequestsCollection.insertOne(agreementRequest)
   res.send(result)
 })
-app.get('/agreementRequests',verifyToken, async (req, res) => {
+app.get('/agreementRequests', verifyToken, async (req, res) => {
   const cursor = await agreementRequestsCollection.find().toArray()
   res.send(cursor)
 })
-app.patch('/agreementRequests/admin/:id',verifyToken,verifyAdmin, async (req, res) => {
+app.patch('/agreementRequests/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
   const id = req.params.id
   const query = { _id: new ObjectId(id) }
   const existingRole = req.body
   const updatedDoc = {
     $set: {
       role: existingRole.role,
-      status:existingRole.status
+      status: existingRole.status
     }
   }
   const result = await agreementRequestsCollection.updateOne(query, updatedDoc)
   res.send(result)
 
 })
+app.get('/agreementRequests', verifyToken, async(req, res) => {
+  const email = req.query.email;
+  console.log('user in the valid token', req.user.email)
 
-app.patch('/accountHolders/admin/:id',verifyToken,verifyAdmin, async (req, res) => {
+  if (email !== req.user.email) {
+    return res.status(403).send({ message: 'Forbidden Access' })
+  }
+  let query = {}
+
+  if (req.query?.email) {
+    query.email = email
+  }
+  if (req.query?.role) {
+    query.role = req.query.role
+  }
+  const result = await agreementRequestsCollection.findOne(query)
+  res.send(result)
+})
+
+app.patch('/accountHolders/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
   const id = req.params.id
   const query = { _id: new ObjectId(id) }
   const existingRole = req.body
@@ -153,25 +172,29 @@ app.patch('/accountHolders/admin/:id',verifyToken,verifyAdmin, async (req, res) 
   res.send(result)
 
 })
-app.get('/accountHolders/admin/:email',verifyToken, async(req,res)=>{
-  const email=req.params.email;
-  if(email!== req.decoded.email){
+app.get('/accountHolders/admin/:email', verifyToken, async (req, res) => {
+  const email = req.params.email;
+  if (email !== req.decoded.email) {
     return res.status(403).send({ message: 'Forbidden Access' })
   }
-  const query= {email:email}
-  const accHolder=await accountHolderCollection.findOne(query)
-  let admin=false;
-  if(accHolder){
-    admin=accHolder?.role==="admin"
+  const query = { email: email }
+  const accHolder = await accountHolderCollection.findOne(query)
+  let admin = false;
+  if (accHolder) {
+    admin = accHolder?.role === "admin"
   }
-  res.send({admin})
+  res.send({ admin })
 })
 
-app.get('/announcements', async (req, res) => {
+app.get('/announcements', verifyToken, async (req, res) => {
   const cursor = await announcementsCollection.find().toArray()
   res.send(cursor)
 })
 
+app.get('/coupons', async (req, res) => {
+  const cursor = await couponsCollection.find().toArray()
+  res.send(cursor)
+})
 app.listen(port, () => {
   console.log(`Apex-Kare-Estate is running on port ${port}`)
 })
